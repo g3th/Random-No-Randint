5b8cf6764da25a79fcf4d378c533a348b99de19b
g3th <theinkandpen@yahoo.co.uk>
2022-06-10 06:20:53 +0100

    new file:   README.MD 	new file:   rand.py

---
+++ rand.py
@@ -0,0 +1,16 @@
+import time
+
+print("\x1bc")
+
+end = int(input('Random range (1-5 Numbers:'))
+start = 13
+
+while True:
+	a = str(time.time())[start:start+end]
+	#print(len(a), end)
+	if len(a) < end:
+		pass
+	else:
+		break
+
+print('Random Number:', int(a))
