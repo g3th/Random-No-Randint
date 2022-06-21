ba392098ae4b6f6dd07ec76c873ea0702edef705
g3th <theinkandpen@yahoo.co.uk>
2022-06-21 02:18:50 +0100

    modified:   rand.py

--- rand.py
+++ rand.py
@@ -6,13 +6,11 @@ end = int(input('Random range: (1-5 Numbers)'))
 start = 10
 
 while True:
-	n = time.time_ns()
 	a = str(time.time_ns())[start:start+end]
 	#print(len(a), end)
 	if '0' in a[0]:
 		pass
 	else:
 		break
-
-print(n)
+		
 print('Random Number:', int(a))
